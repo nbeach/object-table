ec28b8717b4a2d7aef564e7a954704e2014fadf9
Nicholas Beach <nicholascbeach@gmail.com>
2014-09-10 19:05:47 -0400

    Converted configuration to single configuration object

--- src/scripts/directives/ngResourceTable.js
+++ src/scripts/directives/ngResourceTable.js
@@ -6,19 +6,36 @@ angular.module('ngResourceTable', ['ngResourceTableTemplates', 'ngResourceTableS
             scope: {
                 data:'=',
                 columns:'=',
-                rowsPerPage:'@',
-                showPagination:'@',
-                showKeywordSearch:'@'
+                config:'='
             },
-            link:  function($scope, iElement, iAttrs) {
-
-                $scope.filteredData = $scope.data;
-
-                $scope.currentPage = 1;
-                $scope.sortColumn = $scope.columns[0];
-                $scope.sortDescending = false;
-
+            link:  function($scope) {
                 $scope.keywords = "";
+                $scope.currentPage = 1;
+
+                //Initializing values from config object
+                $scope.filteredData = $scope.data;
+                $scope.sortColumn = ($scope.config.defaultSortColumn !== undefined) ? $scope.columns[$scope.config.defaultSortColumn] : $scope.columns[0];
+                $scope.sortDescending = ($scope.config.defaultSortDescending !== undefined) ? $scope.config.defaultSortDescending : false;
+
+                //Set to show all rows on page when pagination is disabled
+                //TODO: Handle this case more elegantly
+                if($scope.config.showPagination !== undefined && $scope.config.showPagination === false){
+                    $scope.config.rowsPerPage = 9007199254740992;
+                }
+
+                $scope.paginationVisible = function()
+                {
+                    return ($scope.config.showPagination !== undefined) ? $scope.config.showPagination : true;
+                };
+
+
+
+                $scope.searchVisible = function()
+                {
+                    return ($scope.config.showSearch !== undefined) ? $scope.config.showSearch : true;
+                };
+
+
 
                 $scope.keywordsChanged = function()
                 {
@@ -33,10 +50,14 @@ angular.module('ngResourceTable', ['ngResourceTableTemplates', 'ngResourceTableS
                     $scope.sortKeepOrder();
                 };
 
+
+
                 $scope.noSearchResults = function()
                 {
                     return $scope.data.length > 0 && $scope.filteredData.length == 0;
                 };
+
+
 
                 $scope.isSortOrder = function(column, sortDescending)
                 {
@@ -45,10 +66,14 @@ angular.module('ngResourceTable', ['ngResourceTableTemplates', 'ngResourceTableS
                         && $scope.sortDescending == sortDescending;
                 };
 
+
+
                 $scope.sortKeepOrder = function()
                 {
                     $scope.sort($scope.sortColumn, $scope.sortDescending);
                 };
+
+
 
                 $scope.sortByColumn = function(column)
                 {
@@ -64,6 +89,8 @@ angular.module('ngResourceTable', ['ngResourceTableTemplates', 'ngResourceTableS
                     }
                 };
 
+
+
                 $scope.sort = function(column, sortDescending)
                 {
                     $scope.sortColumn = column;
@@ -72,26 +99,34 @@ angular.module('ngResourceTable', ['ngResourceTableTemplates', 'ngResourceTableS
                 };
 
 
+
                 $scope.getCurrentPage = function()
                 {
                     if($scope.getPageCount() == 0) return 0;
                     else return $scope.currentPage;
                 };
 
+
+
                 $scope.getPageCount = function()
                 {
-                    return Math.ceil($scope.filteredData.length / $scope.rowsPerPage);
+                    return Math.ceil($scope.filteredData.length / $scope.config.rowsPerPage);
                 };
+
+
 
                 $scope.currentPageIsLast = function()
                 {
                     return $scope.currentPage >= $scope.getPageCount();
                 };
 
+
+
                 $scope.currentPageIsFirst = function()
                 {
                     return $scope.currentPage == 1;
                 };
+
 
 
                 $scope.setPreviousPage = function() {
@@ -100,11 +135,15 @@ angular.module('ngResourceTable', ['ngResourceTableTemplates', 'ngResourceTableS
                     }
                 };
 
+
+
                 $scope.setNextPage = function() {
                     if ($scope.currentPage < $scope.getPageCount()) {
                         $scope.currentPage++;
                     }
                 };
+
+
 
                 $scope.range = function(end, start) {
                     var sequence = [];
@@ -122,10 +161,12 @@ angular.module('ngResourceTable', ['ngResourceTableTemplates', 'ngResourceTableS
                     return sequence;
                 };
 
-                //Returns an array of the indexes of the accounts array that the current page should display
+
+
+                //Returns an array of the indexes of the resources array that the current page should display
                 $scope.getCurrentPageIndexRange = function() {
-                    var pageStartOffset = ($scope.currentPage * $scope.rowsPerPage) - $scope.rowsPerPage;
-                    var pageEndOffset = ($scope.currentPage * $scope.rowsPerPage);
+                    var pageStartOffset = ($scope.currentPage * $scope.config.rowsPerPage) - $scope.config.rowsPerPage;
+                    var pageEndOffset = ($scope.currentPage * $scope.config.rowsPerPage);
 
                     //In case the current page is the last page, and it isn't a full page,
                     //set the page end offset so it isn't beyond the accounts array length
@@ -133,6 +174,11 @@ angular.module('ngResourceTable', ['ngResourceTableTemplates', 'ngResourceTableS
 
                     return $scope.range(pageEndOffset, pageStartOffset);
                 };
+
+
+
+                //Do initial sorting of elements on load
+                $scope.sort($scope.sortColumn, $scope.sortDescending);
 
             }
         };
