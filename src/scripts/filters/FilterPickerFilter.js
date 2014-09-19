f48481ce11b154f9feb8e87e6b8eefdfd1c9b072
Nicholas Beach <nicholascbeach@gmail.com>
2014-09-19 06:56:45 -0400

    Added comment for clarification

--- src/scripts/filters/FilterPickerFilter.js
+++ src/scripts/filters/FilterPickerFilter.js
@@ -3,6 +3,7 @@
     function FilterPickerFilter($filter) {
         return function(value, filterName, filterParam) {
 
+            //The filter may not have been set in the config
             if (filterName === undefined || filterName === null) {
                 return value;
             } else {
