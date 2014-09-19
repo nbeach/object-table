e348b529f8ac4f96107dd9a4b17384eababcb000
Nicholas Beach <nicholascbeach@gmail.com>
2014-09-19 10:21:13 -0400

    Cleaned up formatting and comments

--- src/scripts/services/KeywordParserService.js
+++ src/scripts/services/KeywordParserService.js
@@ -5,7 +5,7 @@
         this.parse = function(keyword) {
             var parts = { column: null, operator: null, keyword: null};
 
-            //If the keyword specifies a column (format [column]:[keyword]
+            //First we see if the keyword specifies a column name (format [column]:[keyword]
             var keywordParts = keyword.split(':');
             if (keywordParts.length == 2) {
                 parts.column = keywordParts[0].toLowerCase();
@@ -13,24 +13,39 @@
                 //If there isn't a keyword after the column name then return null
                 if (keywordParts[1] === undefined) return null;
 
+                //Update the keyword with the text after the column name
                 keyword = keywordParts[1];
             }
 
-            //Separate the keyword from the operator (if an operator exists)
-            if (keyword.substring(0, 2) == '>=' || keyword.substring(0, 2) == '<=') {
+
+            //Next we find if the keyword specifies and operator (format [operator][keyword])
+            if (keyword.substring(0, 2) == '>=' ||
+                keyword.substring(0, 2) == '<=') {
+
                 parts.operator = keyword.substring(0, 2);
                 keyword = keyword.substring(2);
 
-            } else if (keyword.charAt(0) == '=' || keyword.charAt(0) == '<' || keyword.charAt(0) == '>') {
+            } else if (keyword.charAt(0) == '=' ||
+                       keyword.charAt(0) == '<' ||
+                       keyword.charAt(0) == '>') {
+
                 parts.operator = keyword.charAt(0);
+
+                //Update the keyword with the text after the operator
                 keyword = keyword.substring(1);
             }
 
+
+            //Assure that the keyword isn't just whitespace
             keyword = keyword.trim();
-            if (keyword.length > 0) parts.keyword = keyword;
+            if (keyword.length > 0) {
+                parts.keyword = keyword;
+            } else {
+                return null; //Return null if the trimmed keyword is zero length
+            }
 
             return parts;
-        }
+        };
 
     }
 
